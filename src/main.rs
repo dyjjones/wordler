641df3be0c5804449a2af8ac5e415c8b0aaf746e
Dylan Jones <dylan@dylanjones.io>
2022-10-10 18:50:35 -0700

    Clippy Appeasement; fix range and argument length.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 
 const WORD_SIZE: usize = 5;
 
+#[derive(Debug)]
 enum Rule {
     Exclude(char),
     ExcludeAt(char, usize), // excluded at a position, but included in the word
@@ -12,10 +13,10 @@ enum Rule {
 impl Rule {
     fn new(to_exclude: &str) -> Self {
         if to_exclude.len() == 1 {
-            Self::Exclude(to_exclude.chars().nth(0).unwrap())
+            Self::Exclude(to_exclude.chars().next().unwrap())
         } else {
             Self::ExcludeAt(
-                to_exclude.chars().nth(0).unwrap(),
+                to_exclude.chars().next().unwrap(),
                 to_exclude[2..3].parse::<usize>().unwrap(),
             )
         }
@@ -32,11 +33,10 @@ fn check_in(c: char, word: &str, ignore_index: usize) -> bool {
             return true;
         }
     }
-    return false;
+    false
 }
 
 fn char_filter(word: &str, to_exclude: &Vec<Rule>) -> bool {
-    // if
     let word_vec = word.chars().collect::<Vec<_>>();
     for ex in to_exclude {
         match ex {
@@ -53,24 +53,23 @@ fn char_filter(word: &str, to_exclude: &Vec<Rule>) -> bool {
             }
         }
     }
-    return true;
+    true
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
     // [file_path, regex, exclude_letters] = args
-    // let file = File::open(args[0])
     let contents = fs::read_to_string(&args[1])
         .expect("Cannot read file: {&args[1]}")
         .to_lowercase();
-    let contents_filter = contents.split("\n");
+    let contents_filter = contents.split('\n');
 
-    let letters = 'a'..'z';
+    let letters = 'a'..='z';
     let re = Regex::new(&args[2]).unwrap();
 
-    let excluded_chars = if args.len() <= 4 || args[3].len() == 0 {
+    let excluded_chars = if args.len() < 4 || args[3].is_empty() {
         vec![]
     } else {
-        args[3].split(",").map(|s| Rule::new(s)).collect::<Vec<_>>()
+        args[3].split(',').map(Rule::new).collect::<Vec<_>>()
     };
 
     let contents_filter = contents_filter
@@ -79,7 +78,7 @@ fn main() {
             w.chars()
                 .map(|c| letters.contains(&c))
                 .into_iter()
-                .all(|b| b == true)
+                .all(|b| b)
         })
         .filter(|&w| re.is_match(w))
         .filter(|&w| char_filter(w, &excluded_chars))
